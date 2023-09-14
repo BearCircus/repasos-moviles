644a6851d27453427f4e24f2a0a9c0dcd8c41021
ReaAngel9 <raquel.escobedo.9@gmail.com>
2023-09-14 15:57:36 -0600

    Finished home_page look

--- repaso1/lib/home_page.dart
+++ repaso1/lib/home_page.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  String text = '';
+  String iconoFeo = '';
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +15,42 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Tarea 1'),
         ),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Text('BIENVENIDOS', style:  GoogleFonts.pacifico(fontSize: 30, color: Colors.grey),)
-          ),
+        body: Column(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Text('BIENVENIDOS', 
+              style:  GoogleFonts.pacifico(fontSize: 30, color: Colors.grey),)
+            ),
+            Image.asset('assets/Dash_Dart_Black.jpg'),
+            const SizedBox(height: 30),
+            const Text('Seleccione la acción a realizar:',style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+
+            const SizedBox(height: 100),
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Pagina 2'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Pagina 3'),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 50),
+
+            Text('Pg.2 => ${text}'),
+            
+            const SizedBox(height: 50,),
+            
+            Text('Pg.3 => ${iconoFeo}'),
+          ],
+        ),
       ),
     );
   }
--- repaso1/lib/main.dart
+++ repaso1/lib/main.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:repaso1/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
