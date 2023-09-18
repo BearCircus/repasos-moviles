cd1e6ce183ac27a0945c56a223b74aa827b6b621
ReaAngel9 <raquel.escobedo.9@gmail.com>
2023-09-17 22:46:23 -0600

    Graphics for Random page done

--- repaso1/lib/home_page.dart
+++ repaso1/lib/home_page.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'random_number.dart';
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   String text = '';
   String iconoFeo = '';
+  String texto = '';
+
+  Future<void> _showDialog() async {
+    String? inputValue;
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text("Ingrese datos"),
+          content: TextField(
+            onChanged: (value) {
+              inputValue = value;
+            },
+            decoration: InputDecoration(hintText: "Ingrese palabra"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancelar"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Aceptar"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        RandomNumberPage(inputValue: inputValue),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,41 +61,45 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Tarea 1'),
         ),
-        body: Column(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Text('BIENVENIDOS', 
-              style:  GoogleFonts.pacifico(fontSize: 30, color: Colors.grey),)
-            ),
-            Image.asset('assets/Dash_Dart_Black.jpg'),
-            const SizedBox(height: 30),
-            const Text('Seleccione la acción a realizar:',style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-
-            const SizedBox(height: 100),
-            
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Pagina 2'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Pagina 3'),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 50),
-
-            Text('Pg.2 => ${text}'),
-            
-            const SizedBox(height: 50,),
-            
-            Text('Pg.3 => ${iconoFeo}'),
-          ],
+        body: SingleChildScrollView(
+          // <-- Wrap your main content in this
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    'BIENVENIDOS',
+                    style:
+                        GoogleFonts.pacifico(fontSize: 30, color: Colors.grey),
+                  )),
+              Image.asset('assets/Dash_Dart_Black.jpg'),
+              const SizedBox(height: 30),
+              const Text(
+                'Seleccione la acción a realizar:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: _showDialog,
+                    child: const Text('Pagina 2'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Pagina 3'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Text('Pg.2 => ${text}'),
+              const SizedBox(
+                height: 50,
+              ),
+              Text('Pg.3 => ${iconoFeo}'),
+            ],
+          ),
         ),
       ),
     );
