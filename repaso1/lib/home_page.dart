f1d9704792b42717ad433db9a1257ed54dcf6242
Isaac Castillo <isaac.castilloherrera036@gmail.com>
2023-09-17 21:57:04 -0600

    added scrollview

--- repaso1/lib/home_page.dart
+++ repaso1/lib/home_page.dart
@@ -17,39 +17,40 @@ class _MyAppState extends State<MyApp> {
   Future<void> _showDialog() async {
     String? inputValue;
     return showDialog<void>(
-        context: context,
-        builder: (BuildContext dialogContext) {
-          return AlertDialog(
-            title: Text("Ingrese datos"),
-            content: TextField(
-              onChanged: (value) {
-                inputValue = value;
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text("Ingrese datos"),
+          content: TextField(
+            onChanged: (value) {
+              inputValue = value;
+            },
+            decoration: InputDecoration(hintText: "Ingrese palabra"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancelar"),
+              onPressed: () {
+                Navigator.of(context).pop();
               },
-              decoration: InputDecoration(hintText: "Ingrese palabra"),
             ),
-            actions: <Widget>[
-              TextButton(
-                child: Text("Cancelar"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                child: Text("Aceptar"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          RandomNumberPage(inputValue: inputValue),
-                    ),
-                  );
-                },
-              ),
-            ],
-          );
-        });
+            TextButton(
+              child: Text("Aceptar"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        RandomNumberPage(inputValue: inputValue),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -60,41 +61,45 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Tarea 1'),
         ),
-        body: Column(
-          children: [
-            Align(
-                alignment: Alignment.topCenter,
-                child: Text(
-                  'BIENVENIDOS',
-                  style: GoogleFonts.pacifico(fontSize: 30, color: Colors.grey),
-                )),
-            Image.asset('assets/Dash_Dart_Black.jpg'),
-            const SizedBox(height: 30),
-            const Text(
-              'Seleccione la acción a realizar:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 100),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: _showDialog,
-                  child: const Text('Pagina 2'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Pagina 3'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-            Text('Pg.2 => ${text}'),
-            const SizedBox(
-              height: 50,
-            ),
-            Text('Pg.3 => ${iconoFeo}'),
-          ],
+        body: SingleChildScrollView(
+          // <-- Wrap your main content in this
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    'BIENVENIDOS',
+                    style:
+                        GoogleFonts.pacifico(fontSize: 30, color: Colors.grey),
+                  )),
+              Image.asset('assets/Dash_Dart_Black.jpg'),
+              const SizedBox(height: 30),
+              const Text(
+                'Seleccione la acción a realizar:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: _showDialog,
+                    child: const Text('Pagina 2'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Pagina 3'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Text('Pg.2 => ${text}'),
+              const SizedBox(
+                height: 50,
+              ),
+              Text('Pg.3 => ${iconoFeo}'),
+            ],
+          ),
         ),
       ),
     );
